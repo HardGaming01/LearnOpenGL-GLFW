979d21d7b02dd1620117d398caa35b7e17a8ac95
HardGaming <qianyi001@live.cn>
2019-05-14 10:48:46 +0800

    Finished 4.Transform
    Finished 4.Transform and added a little twist to it.

--- src/4.Transform/Transform.cpp
+++ src/4.Transform/Transform.cpp
@@ -1,6 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
 #include <cmath>
@@ -52,6 +55,14 @@ int main()
         processInput(window);
 
         renderUpdate(ourShader, &VAO, texture1, texture2);
+
+        glm::mat4 trans = glm::mat4(1.0f);
+        trans = glm::translate(trans, glm::vec3(sin((float)glfwGetTime()), cos((float)glfwGetTime()), 0.0f));
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(1.0f, 0.0f, 1.0f));
+        trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.0f));
+
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
