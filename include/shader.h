4d41b5c6186908ef14826c7bbfc3c5bc87f74f28
HardGaming <qianyi001@live.cn>
2019-05-15 17:27:22 +0800

    Finished 5.Coordinates
    Finished 5.Coordinates
    Updated the shader class to be able to change uniform matrix

--- include/shader.h
+++ include/shader.h
@@ -12,6 +12,9 @@
 #include <sstream>
 #include <iostream>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 class Shader
 {
 public:
@@ -26,6 +29,7 @@ public:
     void setBool(const std::string &name, bool value) const;
     void setInt(const std::string &name, int value) const;
     void setFloat(const std::string &name, float value) const;
+    void setMat4(const std::string &name, glm::mat4 & value) const;
 };
 
 
@@ -122,6 +126,10 @@ void Shader::setInt(const std::string &name, int value) const
 void Shader::setFloat(const std::string &name, float value) const
 {
     glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+void Shader::setMat4(const std::string &name, glm::mat4 & value)const
+{
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()),1, GL_FALSE, glm::value_ptr(value));
 }
 
 #endif
